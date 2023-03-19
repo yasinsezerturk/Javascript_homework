8f8e7c1a6fac7267136ad3a0e3134c96030097cc
Yasin <yasinsezerturk@gmail.com>
2023-03-19 17:24:09 +0300

    first commit

---
+++ myScript.js
@@ -0,0 +1,50 @@
+//1- Belirlediğimiz sayı aralığında rasgele sayı üreten fonksiyonu yazınız
+
+console.log(Math.floor(Math.random() * 100)); // 0 ve 99 arasında sayı üretir.
+
+// 2- Elimizdeki people dizisiyle people2 yi oluşturalım.
+
+let people = ["Greg", "Mary", "Devon", "James"];
+people.splice(1, 0, "Elizabeth");
+people.splice(3, 1, "Artie");
+let people2 = people.slice();
+console.log(people2);
+
+//3-bu üç veriyi de yanda yazılan çıktılara dönüştüren fonksiyonu yazın. (üç ayrı fonksiyon değil tek fonksiyon istiyoruz.)
+
+function donusturucu(text) {
+  const words = text.split("_");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
+  let nameMail = words.join(" ").split("@")[0];
+  return nameMail;
+}
+console.log(donusturucu("mustafa_selman@gmail.com"));
+console.log(donusturucu("mehmet_colak@hotmail.com"));
+console.log(donusturucu("yasin_sezer@yandex.com"));
+
+// 4- let sayilar = [12,34,2,3,67,66,5,24]
+// a- function maxSecond(sayilar) -> elimizdeki dizinin en büyük ikinci değerini veren fonksiyon
+// b- function minSecond(sayilar) -> elimizdeki dizinin en küçük ikinci değerini veren fonksiyon
+
+// yukarıdaki fonksiyonları oluşturunuz.
+
+let sayilar = [12, 34, 2, 3, 67, 66, 5, 24];
+
+function minSecond(sayilar) {
+  let numbers = sayilar.sort(function (a, b) {
+    return a - b;
+  });
+  return numbers[1];
+}
+
+function maxSecond(sayilar) {
+  let numbers = sayilar.sort(function (a, b) {
+    return b - a;
+  });
+  return numbers[1];
+}
+
+console.log(maxSecond(sayilar));
+console.log(minSecond(sayilar));
